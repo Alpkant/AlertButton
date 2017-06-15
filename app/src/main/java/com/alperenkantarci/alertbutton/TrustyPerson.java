bcb980b510074d5bb3343563c50f3abed46d04e1
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-16 01:32:03 +0300

    Class publicity arranged.

--- app/src/main/java/com/alperenkantarci/alertbutton/TrustyPerson.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/TrustyPerson.java
@@ -4,14 +4,14 @@ package com.alperenkantarci.alertbutton;
  * Created by Alperen Kantarci on 15.06.2017.
  */
 
-public class TrustyPerson {
+class TrustyPerson {
     private String name;
     private String surname;
     private String country_code;
     private String telephone_number;
     private String email;
 
-    public TrustyPerson(String name , String surname, String country_code, String telephone_number, String email) {
+    TrustyPerson(String name, String surname, String country_code, String telephone_number, String email) {
         this.name = name;
         this.surname = surname;
         this.country_code = country_code;
@@ -19,7 +19,7 @@ public class TrustyPerson {
         this.email = email;
     }
 
-    public TrustyPerson() {
+    TrustyPerson() {
 
     }
 
@@ -31,35 +31,35 @@ public class TrustyPerson {
         this.name = name;
     }
 
-    public String getSurname() {
+    String getSurname() {
         return surname;
     }
 
-    public void setSurname(String surname) {
+    void setSurname(String surname) {
         this.surname = surname;
     }
 
-    public String getCountry_code() {
+    String getCountry_code() {
         return country_code;
     }
 
-    public void setCountry_code(String country_code) {
+    void setCountry_code(String country_code) {
         this.country_code = country_code;
     }
 
-    public String getTelephone_number() {
+    String getTelephone_number() {
         return telephone_number;
     }
 
-    public void setTelephone_number(String telephone_number) {
+    void setTelephone_number(String telephone_number) {
         this.telephone_number = telephone_number;
     }
 
-    public String getEmail() {
+    String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
+    void setEmail(String email) {
         this.email = email;
     }
 }
