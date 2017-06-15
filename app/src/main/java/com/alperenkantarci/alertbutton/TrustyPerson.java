fbec7aaaa43b66fe511a66ccece1e61f6440349b
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-15 15:44:48 +0300

    Person object created.

---
+++ app/src/main/java/com/alperenkantarci/alertbutton/TrustyPerson.java
@@ -0,0 +1,53 @@
+package com.alperenkantarci.alertbutton;
+
+/**
+ * Created by Alperen Kantarci on 15.06.2017.
+ */
+
+public class TrustyPerson {
+    private String name;
+    private String surname;
+    private String country_code;
+    private String telephone_number;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getCountry_code() {
+        return country_code;
+    }
+
+    public void setCountry_code(String country_code) {
+        this.country_code = country_code;
+    }
+
+    public String getTelephone_number() {
+        return telephone_number;
+    }
+
+    public void setTelephone_number(String telephone_number) {
+        this.telephone_number = telephone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
