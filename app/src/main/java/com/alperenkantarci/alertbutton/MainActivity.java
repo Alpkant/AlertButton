375dd10f17b887cd071b101b4e7ed3a686b3377d
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-15 16:15:11 +0300

    Click listener added.

--- app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
@@ -1,13 +1,38 @@
 package com.alperenkantarci.alertbutton;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button add_button;
+    Button list_button;
+    ImageView alarm_button;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        add_button = (Button) findViewById(R.id.add_button);
+        list_button = (Button) findViewById(R.id.list_button);
+        alarm_button = (ImageView) findViewById(R.id.alarm_button);
+
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent new_activity = new Intent( getApplicationContext() , AddActivity.class);
+                startActivity(new_activity);
+            }
+        });
+
+        alarm_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "BASILDI", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
