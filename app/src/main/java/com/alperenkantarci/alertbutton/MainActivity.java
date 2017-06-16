5caf3e662313baf319a7499f07bb801689c3aabc
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-17 01:16:32 +0300

    Settings has been added.

--- app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
@@ -1,8 +1,12 @@
 package com.alperenkantarci.alertbutton;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,9 +18,18 @@ public class MainActivity extends AppCompatActivity {
     ImageView alarm_button;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_menu, menu);
+        return true;
+    }
+
+    @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         add_button = (Button) findViewById(R.id.main_add_button);
         list_button = (Button) findViewById(R.id.list_button);
         alarm_button = (ImageView) findViewById(R.id.alarm_button);
