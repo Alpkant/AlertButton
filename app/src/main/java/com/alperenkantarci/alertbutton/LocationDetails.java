476213564189593be952898e263962df992de03a
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-19 16:28:14 +0300

    Location details class initialized.

---
+++ app/src/main/java/com/alperenkantarci/alertbutton/LocationDetails.java
@@ -0,0 +1,14 @@
+package com.alperenkantarci.alertbutton;
+
+/**
+ * Created by Alperen Kantarci on 19.06.2017.
+ */
+
+public class LocationDetails {
+    private double longitude;
+    private double latitude;
+    private double time;
+    private float speed;
+    private float accuracy;
+
+}
