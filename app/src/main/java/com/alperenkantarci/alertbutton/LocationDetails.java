d6dddd56bffad32cc00c2bef6a70b83923af1220
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-19 16:50:23 +0300

    Location details class constructor initialized.

--- app/src/main/java/com/alperenkantarci/alertbutton/LocationDetails.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/LocationDetails.java
@@ -10,5 +10,76 @@ public class LocationDetails {
     private double time;
     private float speed;
     private float accuracy;
+    private String country;
+    private String countryCode;
+    private String Address;
 
+    public LocationDetails(double longitude, double latitude) {
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public float getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public float getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(float accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
 }
