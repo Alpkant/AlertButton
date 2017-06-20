b8be198fe41f8dbe33ca9ba095a1a609394b8009
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-21 01:17:46 +0300

    SMTP protocols  have been added in order to send background mails.

--- app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
@@ -123,6 +123,25 @@ public class MainActivity extends AppCompatActivity {
                             //Request location updates:
                             getTheLocationInfo(MainActivity.this);
 
+
+
+
+
+
+
+                                        try {
+                                            GMailSender sender = new GMailSender("username@gmail.com", "password");
+                                            sender.sendMail("This is Subject",
+                                                    "This is Body",
+                                                    "user@gmail.com",
+                                                    "user@yahoo.com");
+                                        } catch (Exception e) {
+                                            Log.e("SendMail", e.getMessage(), e);
+                                        }
+
+
+                            
+
                             try{
 
                                 if (lastLocation.getLatitude() != 0) {
