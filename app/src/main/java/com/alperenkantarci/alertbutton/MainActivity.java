5a500129d3f6660cc282bab5e6601e4aacbc5d2c
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-20 15:38:32 +0300

    Permission operation is now stable.

--- app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/MainActivity.java
@@ -1,26 +1,16 @@
 package com.alperenkantarci.alertbutton;
 
-import android.*;
 import android.Manifest;
-import android.annotation.TargetApi;
-import android.app.ActionBar;
-import android.app.Activity;
-import android.app.Application;
-import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Build;
-import android.provider.Settings;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -39,13 +29,13 @@ import java.util.List;
 import java.util.Locale;
 
 
-public class MainActivity extends  AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     Button add_button;
     Button list_button;
     ImageView alarm_button;
     FusedLocationProviderClient mFusedLocationClient;
-    double longitude,latitude,time;
-    float speed,accuracy;
+    double longitude, latitude, time;
+    float speed, accuracy;
 
 
     @Override
@@ -61,38 +51,47 @@ public class MainActivity extends  AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
         add_button = (Button) findViewById(R.id.main_add_button);
         list_button = (Button) findViewById(R.id.list_button);
         alarm_button = (ImageView) findViewById(R.id.alarm_button);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 
+            if (checkLocationPermission()) {
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                    //Request location updates:
+                    mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+                    mFusedLocationClient.getLastLocation()
+                            .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                                @Override
+                                public void onSuccess(Location location) {
+                                    if (location != null) {
+
+                                        longitude = location.getLongitude();
+                                        latitude = location.getLatitude();
+                                        time = location.getTime();
+                                        speed = location.getSpeed();
+                                        accuracy = location.getAccuracy();
+
+
+                                        Log.i("Longitude", String.valueOf(longitude));
+                                        Log.i("Latitude", String.valueOf(latitude));
+                                        Log.i("Time", String.valueOf(time));
+                                        Log.i("Speed", String.valueOf(speed));
+
+                                        findAdress(latitude, longitude);
+                                    }
+                                }
+                            });
+
+                }
+            }
 
         }
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        mFusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location location) {
-                        if (location != null) {
-
-                            longitude = location.getLongitude();
-                            latitude = location.getLatitude();
-                            time = location.getTime();
-                            speed = location.getSpeed();
-                            accuracy= location.getAccuracy();
-
-
-                            Log.i("Longitude", String.valueOf(longitude));
-                            Log.i("Latitude", String.valueOf(latitude));
-                            Log.i("Time", String.valueOf(time));
-                            Log.i("Speed", String.valueOf(speed));
-
-                            findAdress(latitude,longitude);
-                        }
-                    }
-                });
 
         add_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -123,7 +122,7 @@ public class MainActivity extends  AppCompatActivity {
         });
     }
 
-    public void findAdress(double latitude , double longitude) {
+    public void findAdress(double latitude, double longitude) {
 
         Geocoder geocoder = new Geocoder(MainActivity.this, Locale.US);
         List<Address> listOfAddress;
@@ -140,13 +139,13 @@ public class MainActivity extends  AppCompatActivity {
 
                 try {
 
-                    Log.i("Country",country);
-                    Log.i("adminArea",adminArea);
-                    Log.i("Locality",locality);
-                    Log.i("FeautureName",feautureName);
-                    Log.i("AddressLine",addressLine);
-                }catch (NullPointerException e){
-                    Log.i("NULL","NULL");
+                    Log.i("Country", country);
+                    Log.i("adminArea", adminArea);
+                    Log.i("Locality", locality);
+                    Log.i("FeautureName", feautureName);
+                    Log.i("AddressLine", addressLine);
+                } catch (NullPointerException e) {
+                    Log.i("NULL", "NULL");
                 }
 
             }
@@ -155,7 +154,101 @@ public class MainActivity extends  AppCompatActivity {
         }
     }
 
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
 
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("We need location permission")
+                        .setMessage("In order to use the app please give permission.")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        1);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // location-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this,
+                            Manifest.permission.ACCESS_FINE_LOCATION)
+                            == PackageManager.PERMISSION_GRANTED) {
+
+                        //Request location updates:
+
+                        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+                        mFusedLocationClient.getLastLocation()
+                                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                                    @Override
+                                    public void onSuccess(Location location) {
+                                        if (location != null) {
+
+                                            longitude = location.getLongitude();
+                                            latitude = location.getLatitude();
+                                            time = location.getTime();
+                                            speed = location.getSpeed();
+                                            accuracy = location.getAccuracy();
+
+
+                                            Log.i("Longitude", String.valueOf(longitude));
+                                            Log.i("Latitude", String.valueOf(latitude));
+                                            Log.i("Time", String.valueOf(time));
+                                            Log.i("Speed", String.valueOf(speed));
+
+                                            findAdress(latitude, longitude);
+                                        }
+                                    }
+                                });
+
+                    }
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+
+                }
+                return;
+            }
+
+        }
+    }
 
 
 }
