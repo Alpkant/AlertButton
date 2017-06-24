30db81b6e9e24b33db34e87d564ae7b19e664a87
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-06-24 15:01:45 +0300

    Optimization.

--- app/src/main/java/com/alperenkantarci/alertbutton/FragmentActivity.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/FragmentActivity.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceCategory;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceScreen;
 import android.support.annotation.Nullable;
