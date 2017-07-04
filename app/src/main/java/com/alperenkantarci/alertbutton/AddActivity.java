ef9afeb8f897dfe0e4a0d90772c715a17cca82ff
Alperen Kantarci <kantarcia@itu.edu.tr>
2017-07-04 15:32:02 +0300

    Taking name from contact has implemented.

--- app/src/main/java/com/alperenkantarci/alertbutton/AddActivity.java
+++ app/src/main/java/com/alperenkantarci/alertbutton/AddActivity.java
@@ -1,9 +1,14 @@
 package com.alperenkantarci.alertbutton;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.provider.ContactsContract;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,7 +22,13 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.R.attr.data;
+
+
 public class AddActivity extends AppCompatActivity {
+
+
+    static final int PICK_CONTACT = 1;
 
     EditText name, surname, phoneNumber, email;
     Spinner country_codes;
@@ -81,10 +92,8 @@ public class AddActivity extends AppCompatActivity {
                 if (name.getText().toString().equals("")) {
                     Toast.makeText(AddActivity.this, "Name can't be empty.Please enter a name.", Toast.LENGTH_SHORT).show();
                     return;
-                } else if (surname.getText().toString().equals("")) {
-                    Toast.makeText(AddActivity.this, "Surname can't be empty.Please enter a surname.", Toast.LENGTH_SHORT).show();
-                    return;
-                } else if (phoneNumber.getText().toString().equals("") && email.getText().toString().equals("")) {
+                }
+                else if (phoneNumber.getText().toString().equals("") && email.getText().toString().equals("")) {
                     Toast.makeText(AddActivity.this, "Either email or phone number should be given in order to add a person.", Toast.LENGTH_SHORT).show();
                     return;
                 }
@@ -124,12 +133,49 @@ public class AddActivity extends AppCompatActivity {
         add_from_contacts_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, PICK_CONTACT);
 
             }
+
         });
+
+
     }
 
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
 
+        switch (reqCode) {
+            case (PICK_CONTACT):
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri resultUri = data.getData();
+                    Cursor cont = getContentResolver().query(resultUri, null, null, null, null);
+                    if (!cont.moveToNext()) {
+                        Toast.makeText(this, "Cursor contains no data", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    int columnIndexForId = cont.getColumnIndex(ContactsContract.Contacts._ID);
+                    String contactId = cont.getString(columnIndexForId);
+
+                    // Fetch contact name with a specific ID
+                    String whereName = ContactsContract.Data.MIMETYPE + " = ? AND " + ContactsContract.CommonDataKinds.StructuredName.CONTACT_ID + " = " + contactId;
+                    String[] whereNameParams = new String[] { ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE };
+                    Cursor nameCur = getContentResolver().query(ContactsContract.Data.CONTENT_URI, null, whereName, whereNameParams, ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME);
+                    while (nameCur.moveToNext()) {
+                        String given = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.GIVEN_NAME));
+                        String family = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.FAMILY_NAME));
+                        String display = nameCur.getString(nameCur.getColumnIndex(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME));
+                        Toast.makeText(this, "Name: " + given + " Family: " +  family + " Displayname: "  + display, Toast.LENGTH_LONG).show();
+                    }
+                    nameCur.close();
+                    cont.close();
+
+                }
+                break;
+        }
+    }
 
 
 }
